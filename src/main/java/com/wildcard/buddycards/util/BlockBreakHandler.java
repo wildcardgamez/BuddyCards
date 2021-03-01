0fb33921facb1d9620178912dafa0ea88b30c71a
Allen <allenseitz@gmail.com>
2021-03-01 17:24:34 -0500

    Display Breaking Tweaks
    Allow creative mode players to break locked stands. Also send the same message to survival mode players that they would receive for activating the locked stand, too.

--- src/main/java/com/wildcard/buddycards/util/BlockBreakHandler.java
+++ src/main/java/com/wildcard/buddycards/util/BlockBreakHandler.java
@@ -6,6 +6,8 @@ import com.wildcard.buddycards.blocks.CardStandBlock;
 import com.wildcard.buddycards.blocks.CardStandTile;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -16,12 +18,21 @@ public class BlockBreakHandler
     {
     	BlockState targetBlock = event.getWorld().getBlockState(event.getPos());
     	
+    	// I think it is possible for the player to be null if something like the EnderDragon is doing this?
+    	PlayerEntity player = event.getPlayer();
+    	if ( player == null )
+    	{
+    		return;
+    	}
+    	
 		// this makes a locked card display absolutely indestructable 
     	if (targetBlock.getBlock() instanceof CardDisplayBlock)
     	{
     		CardDisplayTile tile = (CardDisplayTile)event.getWorld().getTileEntity(event.getPos());
-    		if (tile.isLocked() )
+    		    		
+    		if (tile.isLocked() && !player.isCreative())
     		{
+                player.sendStatusMessage(new TranslationTextComponent("block.buddycards.card_display.lock"), true);
             	event.setCanceled(true);
     		}
     	}
@@ -30,8 +41,9 @@ public class BlockBreakHandler
     	if (targetBlock.getBlock() instanceof CardStandBlock)
     	{
     		CardStandTile tile = (CardStandTile)event.getWorld().getTileEntity(event.getPos());
-    		if (tile.isLocked() )
+    		if (tile.isLocked() && !player.isCreative())
     		{
+                player.sendStatusMessage(new TranslationTextComponent("block.buddycards.card_display.lock"), true);
             	event.setCanceled(true);
     		}
     	}    	
