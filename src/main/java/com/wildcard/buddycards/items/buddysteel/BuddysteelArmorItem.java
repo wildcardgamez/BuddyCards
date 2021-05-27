6552c9fdd03ada6e65b67124d36f0a4ab5e1a9a0
wildcardgamez <75537188+wildcardgamez@users.noreply.github.com>
2021-05-27 09:52:35 -0400

    Fix server crashing bug

--- src/main/java/com/wildcard/buddycards/items/buddysteel/BuddysteelArmorItem.java
+++ src/main/java/com/wildcard/buddycards/items/buddysteel/BuddysteelArmorItem.java
@@ -19,8 +19,12 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
 
 public class BuddysteelArmorItem extends ArmorItem {
     private static final UUID[] ARMOR_MODIFIERS = new UUID[]{UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"), UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"), UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"), UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
@@ -61,6 +65,7 @@ public class BuddysteelArmorItem extends ArmorItem {
         return multimap;
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Nullable
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
@@ -71,11 +76,12 @@ public class BuddysteelArmorItem extends ArmorItem {
         return super.getArmorTexture(stack, entity, slot, type);
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Nullable
     @Override
     public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType armorSlot, A _default) {
         if (material.equals(BuddysteelArmorMaterial.PERFECT_BUDDYSTEEL) || material.equals(BuddysteelArmorMaterial.ZYLEX))
             return (A) new PerfectBuddysteelArmorModel(slot);
-        return super.getArmorModel(entityLiving, itemStack, armorSlot, _default);
+        return _default;
     }
 }
