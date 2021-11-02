b7396c093a18de95e8a8b2c1a026e6f15b8485a4
wildcardgamez <75537188+wildcardgamez@users.noreply.github.com>
2021-11-02 14:19:40 -0400

    Fix Buddysteel and BoosterBoxes

--- src/main/java/com/wildcard/buddycards/blocks/BoosterBoxBlock.java
+++ src/main/java/com/wildcard/buddycards/blocks/BoosterBoxBlock.java
@@ -4,10 +4,14 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
@@ -19,17 +23,20 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
 
 public class BoosterBoxBlock extends Block {
+    public static final DirectionProperty DIR = BlockStateProperties.HORIZONTAL_FACING;
     public static final IntegerProperty LAYERS = IntegerProperty.create("layers", 1, 4);
     protected static final VoxelShape[] SHAPE_BY_LAYER = new VoxelShape[]{VoxelShapes.empty(), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
 
     public BoosterBoxBlock(int setNumber, String modId) {
-        super(Properties.copy(Blocks.STONE));
-        this.registerDefaultState(this.stateDefinition.any().setValue(LAYERS, Integer.valueOf(1)));
+        super(Properties.copy(Blocks.WHITE_WOOL));
+        this.registerDefaultState(this.stateDefinition.any().setValue(LAYERS, Integer.valueOf(1)).setValue(DIR, Direction.NORTH));
         SET_NUMBER = setNumber;
         SPECIFIC_MOD = modId;
     }
@@ -81,12 +88,12 @@ public class BoosterBoxBlock extends Block {
             int i = blockstate.getValue(LAYERS);
             return blockstate.setValue(LAYERS, Math.min(4, i + 1));
         } else {
-            return super.getStateForPlacement(context);
+            return super.getStateForPlacement(context).setValue(DIR, context.getHorizontalDirection());
         }
     }
 
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(LAYERS);
+        builder.add(LAYERS).add(DIR);
     }
 
     @Override
@@ -94,5 +101,13 @@ public class BoosterBoxBlock extends Block {
         if(!ModList.get().isLoaded(SPECIFIC_MOD))
             return;
         super.fillItemCategory(group, items);
+    }
+
+    @Override
+    public boolean removedByPlayer(BlockState state, World world, BlockPos pos, PlayerEntity player, boolean willHarvest, FluidState fluid) {
+        if (!player.isCreative()) {
+            InventoryHelper.dropItemStack(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(state.getBlock().asItem(), state.getValue(LAYERS)));
+        }
+        return super.removedByPlayer(state, world, pos, player, willHarvest, fluid);
     }
 }
